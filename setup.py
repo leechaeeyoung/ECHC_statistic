eb75aa37a516da9809394a8cdcba797449374256
leechaeyoung <zs1397@naver.com>
2024-10-24 10:59:04 +0900

    배포 error로 setup 수정

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Fcstatistics',
-    version='0.0.1',
+    version='0.0.4',
     description='Fc statistics algorithm',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -10,13 +10,10 @@ setup(
     author_email='fourchainsrd@gmail.com',
     url='https://github.com/leechaeeyoung/Fc',
     packages=find_packages(),
-    install_requires=[
-        'package1>=0.0.0',
-    ],
+    install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License'
     ],
     python_requires='>=3.6',
 )
