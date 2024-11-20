4ebfe9e0e0477995bfdefbda8b7af28652cbc8f8
leechaeyoung <zs1397@naver.com>
2024-11-20 15:01:39 +0900

    0.0.9

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
@@ -9,15 +10,18 @@ extensions = [
 # setup 함수 정의
 setup(
     name="MetaMorphic",
-    version="0.0.2",
+    version="0.0.9",
     author='fourchains_R&D',
     author_email='fourchainsrd@gmail.com',
     url='https://github.com/leechaeeyoung/Fc', 
     packages=['MetaMorphic'],
     long_description=open("README.md", encoding="utf-8").read(),
-    ext_modules=cythonize(extensions, compiler_directives={'language_level': "3"}),    # Cython 컴파일 활성화
+    ext_modules=cythonize(extensions, compiler_directives={'language_level': "3"}),  # Cython 컴파일 활성화
     package_data={
-        "MetaMorphic": ["*.pxd", "*.c", "*.h", "*.pyd"],  # 포함할 파일 확장자 명시
+        "MetaMorphic": ["*.pxd", "*.c", "*.h", "*.pyd"],
     },
     include_package_data=True,
+    exclude_package_data={
+        "MetaMorphic": ["*.py", "*.pyx"],  # .py와 .pyx 파일 제외
+    },
 )
