02875722084e0e3800703ecf3fac99d241950a66
leechaeyoung <zs1397@naver.com>
2024-11-21 12:12:17 +0900

    0.0.18 final

--- Metamorphic/__init__.py
+++ Metamorphic/__init__.py
@@ -1,13 +1,4 @@
 
-__version__='0.0.17'
+__version__='0.0.18'
 
-import pkg_resources
-import pandas as pd
-
-# pkg_resources로 설치된 패키지 안의 절대 경로 가져오기
-csv_path = pkg_resources.resource_filename('Metamorphic', 'data/표준정규분포표.csv')
-
-# CSV 파일 읽기
-df = pd.read_csv(csv_path)
-print(df)
-
+from .MorphAlyt import *
